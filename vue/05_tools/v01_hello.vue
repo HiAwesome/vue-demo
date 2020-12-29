16f9e585340fe3ff28f923b2ca20f699d58c59ac
moqi <moqimoqidea@gmail.com>
2020-12-29 10:11:17 +0800

    Add 单文件组件 and v01_hello.vue

---
+++ vue/05_tools/v01_hello.vue
@@ -0,0 +1,24 @@
+<!-- 模板部分 -->
+<template>
+  <p>{{ greeting }} World!</p>
+</template>
+
+<!-- 脚本部分 -->
+<script>
+export default {
+  name: "v01_hello",
+  data: function () {
+    return {
+      greeting: 'Hello'
+    }
+  }
+}
+</script>
+
+<!-- 样式部分 -->
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
