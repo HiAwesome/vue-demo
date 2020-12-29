1ee4bee00919cfc241e5a60d13ace137331b6a43
moqi <moqimoqidea@gmail.com>
2020-12-29 10:25:34 +0800

    引入其他组件

--- vue/05_tools/v01_hello.vue
+++ vue/05_tools/v01_hello.vue
@@ -1,22 +1,33 @@
 <!-- 模板部分 -->
-<template>
-  <p>{{ greeting }} World!</p>
+<template lang="jade">
+div
+  p {{
+    greeting
+  }} World!
+   other-component
+
 </template>
 
 <!-- 脚本部分 -->
 <script>
+/* 引入其他组件 */
+import OtherComponent from './v02_other_component'
+
 export default {
   name: "v01_hello",
   data: function () {
     return {
       greeting: 'Hello'
     }
+  },
+  components: {
+    OtherComponent
   }
 }
 </script>
 
 <!-- 样式部分 -->
-<style scoped>
+<style lang="stylus" scoped>
 p {
   font-size: 2em;
   text-align: center;
---
+++ vue/05_tools/v02_other_component.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "v02_other_component"
+}
+</script>
+
+<style scoped>
+
+</style>
